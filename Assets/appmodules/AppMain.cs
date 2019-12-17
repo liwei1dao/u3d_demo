215821ffb354112eaa77d993e4c95acf47b8be9e
liwei1dao <2211068034@qq.com>
2019-12-17 16:42:20 +0800

    提交初始代码

---
+++ Assets/appmodules/AppMain.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using u3d_fw;
+
+public class AppMain : Main
+{
+    protected override void StartApp()
+    {
+        base.StartApp();
+        Manager_ManagerModel.Instance.StartModel<ResourceModule>((mpdule) =>
+        {
+            Manager_ManagerModel.Instance.StartModel<LuaManagerModule>();
+        });
+    }
+}
