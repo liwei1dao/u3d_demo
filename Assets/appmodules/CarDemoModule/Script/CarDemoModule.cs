b2e23731eb9fb0075e990b21eea8e055f749056b
liwei1dao <2211068034@qq.com>
2020-01-15 14:58:19 +0800

    同步底层库

--- Assets/appmodules/CarDemoModule/Script/CarDemoModule.cs
+++ Assets/appmodules/CarDemoModule/Script/CarDemoModule.cs
@@ -1,9 +1,26 @@
 ﻿using u3d_fw;
-
+using UnityEngine;
 public class CarDemoModule : ManagerContorBase<CarDemoModule>
 {
+    [u3dfw_Serialize]
+    private string demostr1 = "hello";
+
+    [u3dfw_SerializeName("我是 hello2")]
+    private string demostr2 = "hello";
+
     public override void Load(params object[] agr)
     {
         base.Load(agr);
     }
+
+
+    public override void ShowInspector() {
+        GUILayout.BeginVertical();
+        GUILayout.Label("liwei1dao");
+        if (GUILayout.Button("按钮")) {
+            Debug.Log("测试自定义属性面板");
+        }
+        GUILayout.EndVertical();
+    }
+
 }
